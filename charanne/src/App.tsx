b8661a4c38fe7c5004aa4a8d2c789116eacbeec1
goremi oguru <goremioguru@gmail.com>
2025-12-26 21:47:39 +0100

    Initial commit

---
+++ charanne/src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    
+      <div>
+        <h1 className="text-3xl font-bold text-red-500 underline">
+        Tailwind v4 is working!
+        </h1>
+      </div>
+      
+  )
+}
+
+export default App
